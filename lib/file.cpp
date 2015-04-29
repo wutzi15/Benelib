55558650536c5d01b142e737fe4a9c6f8e3f1f5d
Benedikt Bergenthal <b.bergenthal@cern.ch>
2015-04-29 19:19:23 +0200

    added boost filesystem checks
    Signed-off-by: Benedikt Bergenthal <b.bergenthal@cern.ch>

--- lib/file.cpp
+++ lib/file.cpp
@@ -1,9 +1,15 @@
 #include "bene_file.h"
 #include "bene_quickRoot.h"
 #include "TCanvas.h"
+#include "boost/filesystem.hpp"
 
 TH1F bene::plotHistFromFile(const std::string & s)
 {
+    if(!boost::filesystem::exists(boost::filesystem::path(s))) {
+        std::cerr <<  __PRETTY_FUNCTION__ << std::endl;
+        std::cerr << "File to open (" << s << ") does not exist\n";
+        return TH1F();
+    }
     auto v = bene::readToVector<double, 3, 1, 2>(s);
     auto h = bene::quickHist<double, 100,2>(v);
     TCanvas c("","", 500, 500);
@@ -13,6 +19,11 @@ TH1F bene::plotHistFromFile(const std::string & s)
 
 TGraph bene::plotTGraphFromFile(const std::string & s)
 {
+    if(!boost::filesystem::exists(boost::filesystem::path(s))) {
+        std::cerr <<  __PRETTY_FUNCTION__ << std::endl;
+        std::cerr << "File to open (" << s << ") does not exist\n";
+        return TGraph();
+    }
     auto v = bene::readToVector<double, 3, 1, 2>(s);
     auto g = bene::quickTGraph<double, 1,2>(v);
     TCanvas c("","", 500, 500);
@@ -22,6 +33,11 @@ TGraph bene::plotTGraphFromFile(const std::string & s)
 
 TGraphErrors bene::plotTGraphErrorsFromFile(const std::string & s)
 {
+    if(!boost::filesystem::exists(boost::filesystem::path(s))) {
+        std::cerr <<  __PRETTY_FUNCTION__ << std::endl;
+        std::cerr << "File to open (" << s << ") does not exist\n";
+        return TGraphErrors();
+    }
     auto v = bene::readToVector<double, 3, 1, 2,3>(s);
     auto g = bene::quickTGraphError<double, 1,2,3>(v);
     TCanvas c("","", 500, 500);
