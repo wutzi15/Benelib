93cc3961f5d9cfda74404fdf5549ece7114beb0a
Benedikt Bergenthal <b.bergenthal@cern.ch>
2015-04-29 12:41:49 +0200

    Added quick hist and graph methods
    Signed-off-by: Benedikt Bergenthal <b.bergenthal@cern.ch>

--- include/algorithm.h
+++ include/algorithm.h
@@ -1,6 +1,35 @@
 #ifndef BENE_ALGORITHM
 #define BENE_ALGORITHM 
+#include <vector>
+#include <algorithm>
+#include <tuple>
+
+#include "TGraph.h"
+#include "TGraphErrors.h"
+#include "TH1F.h"
+#include "TROOT.h"
+
+
 namespace bene {
 	
+	template<typename T>
+	T min(const std::vector<T> &v){
+		return *std::min_element(v.begin(), v.end());
+	}
+
+	template<typename T>
+	T max(const std::vector<T> &v){
+		return *std::max_element(v.begin(), v.end());
+	}
+
+	template<typename T, int X, int Y>
+	TGraph quickTGraph(const std::vector<T> & v);
+
+	template<typename T, int X, int Y, int Err>
+	TGraphErrors quickTGraphError(const std::vector<T> & v);
+
+	template<typename T, int bins> 
+	TH1F quickHist(const std::vector<T> & v);
 }
+
 #endif
--- lib/algorithm.cpp
+++ lib/algorithm.cpp
@@ -1,1 +1,40 @@
 #include "algorithm.h"
+
+template<typename T, int X, int Y>
+TGraph bene::quickTGraph(const std::vector<T> & v)
+{
+	const size_t num_elem = v.size();
+	TGraph g(num_elem);
+	for (size_t i = 0; i < num_elem; ++i)
+	{
+		g.SetPoint(i, std::get<X>(v), std::get<Y>(v));
+	}
+	return g;
+}
+
+template<typename T, int X, int Y, int Err>
+TGraph bene::quickTGraphError(const std::vector<T> & v)
+{
+	const size_t num_elem = v.size();
+	TGraphErrors g(num_elem);
+	for (size_t i = 0; i < num_elem; ++i)
+	{
+		g.SetPoint(i, std::get<X>(v), std::get<Y>(v));
+		g.SetPointError(i, std::get<Err>(v));
+	}
+	return g;
+}
+
+template<typename T, int bins> 
+TH1F bene::quickHist(const std::vector<T> & v) 
+{
+	const size_t num_elem = v.size();
+	double min_v = min(v);
+	double min = min_v < 0 ? min_v * 1.1 : min_v * 0.9;
+	double max_v = max(v);
+	double max = max_v < 0 ? max_v * 0.9 : max_v * 1.1;
+	TH1F h("", "", bins, min, max);
+	for(const auto & e : v) {
+		h.Fill(e);
+	}
+}
